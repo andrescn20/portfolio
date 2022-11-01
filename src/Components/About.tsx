d20aac8eaa92c696893ccda14b2ffe213b7ba79e
Andres C <andrescn20@gmail.com>
2022-11-01 12:23:59 -0600

    Initial About Styling. Project Section Structure

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>Hello there. I'm Andres: Front End Developer. </h2>
-      <h3>About Me:</h3>
+    <div className='about'>
+      <h2>
+        Hello there. I'm Andres:<br></br> Front End Developer.{' '}
+      </h2>
+
       <p>
         Im a self thaught web Developer with a strong background in Engineering
         and Education.
--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const Projects = () => {
-  return <p>This is Projects Section</p>;
+  return (
+    <div className='projects'>
+      <h2>Projects</h2>
+      <div className='projectContainer'>
+        <h3>Calion Crafting</h3>
+        <p>E-commerce Shop</p>
+      </div>
+      <div className='projectContainer'>
+        <h3>NPC Weather </h3>
+        <p> MMORPG themed Weather App</p>
+      </div>
+      <div className='projectContainer'>
+        <h3>Newton Project</h3>
+        <p>Education: Teaches how to clear variables</p>
+      </div>
+      <div className='projectContainer'>
+        <h3>BodyBuilding Tracker</h3>
+        <p>App for tracking and designing workout cycles</p>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
