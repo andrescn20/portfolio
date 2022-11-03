aa04aeabc1ca7e4cdc0d4888646beb0ef3d37fbb
Andres C <andrescn20@gmail.com>
2022-11-03 08:53:19 -0600

    Add functionality to mail form

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact: React.FC<{ updatePosition: (position: number) => void }> = ({
   updatePosition,
@@ -10,13 +11,39 @@ const Contact: React.FC<{ updatePosition: (position: number) => void }> = ({
     updatePosition(contactPosition - 70);
   }, [updatePosition]);
 
+  const form = useRef<any>('');
+
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_8kltq91',
+        'template_8hqynwp',
+        form.current,
+        'H4w7hs6ZBHTi8EtuE'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className='contact' id='contact' ref={contactRef}>
       <h2> Contact Me</h2>
-      <form>
-        <input type='text' placeholder='Name'></input>
-        <input type='text' placeholder='Type your message here.'></input>
-        <button type='submit'>Send Email</button>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type='text' name='user_name' />
+        <label>Email</label>
+        <input type='email' name='user_email' />
+        <label>Message</label>
+        <textarea name='message' />
+        <input type='submit' value='Send' />
       </form>
     </div>
   );
