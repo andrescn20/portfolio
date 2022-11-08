4fb8ddebb6b9135511d94e2454d672b0bf875c01
Andres C <andrescn20@gmail.com>
2022-11-07 20:44:47 -0600

    Comment out incomplete projects

--- src/projects.js
+++ src/projects.js
@@ -23,28 +23,28 @@ export const projects = [
     image: require('./Videos/CalionCrafting.gif'),
     id: 1,
   },
-  {
-    name: 'Newton Project',
-    description:
-      'Educational app meant to teach students how to correctly solve an equation. Directed to aspiring engineering and science students.',
-    techStack: [
-      {
-        name: 'React',
-        link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
-      },
-      {
-        name: 'Sass',
-        link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg',
-      },
-      {
-        name: 'TypeScript',
-        link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
-      },
-    ],
-    links: { gitHub: '', live: '' },
-    image: 'image',
-    id: 2,
-  },
+  // {
+  //   name: 'Newton Project',
+  //   description:
+  //     'Educational app meant to teach students how to correctly solve an equation. Directed to aspiring engineering and science students.',
+  //   techStack: [
+  //     {
+  //       name: 'React',
+  //       link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+  //     },
+  //     {
+  //       name: 'Sass',
+  //       link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg',
+  //     },
+  //     {
+  //       name: 'TypeScript',
+  //       link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+  //     },
+  //   ],
+  //   links: { gitHub: '', live: '' },
+  //   image: 'image',
+  //   id: 2,
+  // },
   {
     name: 'NPC Weather',
     description:
@@ -63,22 +63,22 @@ export const projects = [
     image: require('./Videos/NPCWeather.gif'),
     id: 3,
   },
-  {
-    name: 'BodyBuilding in depth Planning App',
-    description:
-      "Creating a rock solid routine requires more than targeting different muscles throught the week. Its crucial to take into consideration the total volume for each body part and keep everything whithin the optimal ranges. Thats were 'App Name' comes in. ",
-    techStack: [
-      {
-        name: 'React',
-        link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
-      },
-      {
-        name: 'Sass',
-        link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg',
-      },
-    ],
-    links: { gitHub: '', live: '' },
-    image: 'image',
-    id: 4,
-  },
+  // {
+  //   name: 'BodyBuilding in depth Planning App',
+  //   description:
+  //     "Creating a rock solid routine requires more than targeting different muscles throught the week. Its crucial to take into consideration the total volume for each body part and keep everything whithin the optimal ranges. Thats were 'App Name' comes in. ",
+  //   techStack: [
+  //     {
+  //       name: 'React',
+  //       link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+  //     },
+  //     {
+  //       name: 'Sass',
+  //       link: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg',
+  //     },
+  //   ],
+  //   links: { gitHub: '', live: '' },
+  //   image: 'image',
+  //   id: 4,
+  // },
 ];
